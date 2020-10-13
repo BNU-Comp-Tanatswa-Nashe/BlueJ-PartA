b0a1de2c19ffbc1d472b1fd7a317f3a50aab2afe
@Tanatswa-Nashe <72219219+Tanatswa-Nashe@users.noreply.github.com>
2020-10-13 16:34:23 +0100

    Added course and class

---
+++ app01-lab-classes/Course.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Course here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Course
+{
+    private String title;
+    private String codeNo;
+    
+    
+    public Course()
+    {
+        
+    }
+    
+}
