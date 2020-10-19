bbf48ee763adae3b15fc800cae358b2f70f0c699
@Tanatswa-Nashe <72219219+Tanatswa-Nashe@users.noreply.github.com>
2020-10-19 23:00:30 +0100

    added time,day and instructor

--- app01-lab-classes/Course.java
+++ app01-lab-classes/Course.java
@@ -1,13 +1,15 @@
 
 /**
- * Write a description of class Course here.
+ * This class will store information about university course students will apply for.
+ * The course will have an official title and a UCAS course code for the students.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Tanatswa-Nashe Rusike
+ * @version 19/10/2020
  */
 public class Course
 {
     private String title;
+    
     private String codeNo;
     
     /**
--- app01-lab-classes/Student.java
+++ app01-lab-classes/Student.java
@@ -19,6 +19,7 @@ public class Student
     // A BSc course has 120 credits, each module has 15 Credits
     private int credits;
 
+    private Course course;
     /**
      * Create a new student with a given name and ID number.
      */
